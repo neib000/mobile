09f481a97f8ee59eeec350e5d44a4fec9ec93695
zuradel <truongbien.qh@gmail.com>
2020-08-06 14:08:30 +0700

    thêm chức năng tìm kiếm

--- js/main.js
+++ js/main.js
@@ -9,10 +9,12 @@ function xuLyLayDanhSachDienThoai() {
 xuLyLayDanhSachDienThoai();
 var danhSachDienThoai = [];
 
-function renderHTML() {
+const renderHTML = function(arr) {
+
+  arr = arr || danhSachDienThoai;
   var htmlcontent = "";
-  for (var i = 0; i < danhSachDienThoai.length; i++) {
-    var dienThoai = danhSachDienThoai[i];
+  for (var i = 0; i < arr.length; i++) {
+    var dienThoai = arr[i];
     htmlcontent += `
           <li class="item">
             <div class="item__info">
@@ -39,4 +41,31 @@ function numberWithCommas(x) {
   var parts = x.toString().split(".");
   parts[0] = parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ".");
   return parts.join(".");
+}
+//Hàm tìm kiếm
+const findEmpl = function () {
+  const foundedEmpl = [];
+  // 1. Lấy keyword người dùng nhaao vào (dom)
+  const keyword = document
+    .getElementById("txtSearch")
+    .value.trim()
+    .toLowerCase();
+  // 2. Tìm theo mã: lập for, kiểm tra từng nhân viên trong mảng, cớ nhân viên nào
+  // có id giống với kywword => push NV đó vào foundedEmpl
+  //3. console.log(foundedEmpl)
+  for (var i = 0; i < danhSachDienThoai.length; i++) {
+    const currentEmp = danhSachDienThoai[i];
+    var fullName = currentEmp.name;
+
+    fullName = fullName.toLowerCase();
+    // console.log(currentEmp.id);
+    if (currentEmp.id === keyword) {
+      foundedEmpl.push(currentEmp);
+      break;
+    }
+    if (fullName.indexOf(keyword) !== -1) {
+      foundedEmpl.push(currentEmp);
+    }
+  }
+  renderHTML(foundedEmpl);
 }
