489f70d91d60619f50e4f09adb4335697cb8f8ce
zuradel <truongbien.qh@gmail.com>
2020-08-08 02:08:14 +0700

    sửa Mobile.js cho giống với api

--- js/models/Mobile.js
+++ js/models/Mobile.js
@@ -1,7 +1,9 @@
-function Mobile(tenDT, hinhAnh, hangDT, giaTien, khoHang){
-    this.tenDT = tenDT;
-    this.hinhAnh = hinhAnh;
-    this.hangDT = hangDT;
-    this.giaTien = giaTien;
-    this.khoHang = khoHang;
+function Mobile(id, name, image, description, price, invetory, type){
+    this.id = id;
+    this.name = name;
+    this.image = image;
+    this.description = description;
+    this.price = price;
+    this.invetory = invetory;
+    this.type = type;
 }
